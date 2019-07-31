b1e1205345003da7e98ba19b34ba53c59195a197
must_eat <mss@tutanota.de>
2019-07-31 18:30:35 +0300

    updated: palemoon build script and mozconfig.

--- pkg-management/build-configs/browser/mightyluna/build-mightyluna.sh
+++ pkg-management/build-configs/browser/mightyluna/build-mightyluna.sh
@@ -1,47 +1,90 @@
 #!/bin/env bash
-if [ -z $1 ]; then echo " ! no archive selected."; exit 1; fi
+# a >> check if a tarball is selected
+if	[ -z $1 ];		then printf "\n ! no archive selected, exiting.\n\n";	exit 1
+elif	[ ! -f "${PWD}/${1}" ];	then printf "\n ! tarball doesn't exist, exiting.\n\n";	exit 1
+fi
 
+# b >> check if $1 is a palemoon tarball
+case $1 in
+ *PM*)
+  printf " * archive is a palemoon source tarball.\n\n" ;;
+ *)
+  printf " * ${1} is not a palemoon source tarball, exiting.\n"
+  exit 1 ;;
+esac
+
+# c >> set build vars
 arname=$(echo UXP-$(echo $1 | sed 's/.tar.gz//'))			&&
 repodir="/mss/repo/pkg-management/build-configs/browser/mightyluna"	&&
 srcdir=/mss/work/UXP							&&
 
-printf "\n * extracting ${1}.\n"
+# d >> action
+# d1 >> extract tarball
+printf " * extracting ${1}."						&&
 tar xf $1								&&
+printf "\t--> done.\n"							&&
 
-printf "\n * renaming dirs.\n"						&&
-mv -v "${arname}" "/mss/work/UXP"					&&
-
+# d2 >> mv to /mss/work/UXP
+printf " * renaming dirs."						&&
+mv "${arname}" "/mss/work/UXP"						&&
+printf "\t\t\t--> done.\n"						&&
 cd "${srcdir}"								&&
 
-printf "\n * applying mightyluna branding patch.\n"			&&
-patch -p1 < "${repodir}"/0001-mightyluna-branding-28.6.1.patch		&&
+# d3 >> apply mightyluna branding
+printf " * applying mightyluna branding patch."				&&
+patch -p1 < "${repodir}"/0001-mightyluna-branding-28.6.1.patch \
+  &> /tmp/mightyluna-build.log						&&
+printf "\t--> done.\n"							&&
 
-printf "\n * applying gcc9.1 patch.\n"					&&
-patch -p1 < "${repodir}"/0002-palemoon-gcc9.1.patch			&&
-
-printf "\n * copying mozconfig.\n"
-cp -v "${repodir}"/mightyluna-mozconfig-syslibs.config \
+# d4 >> copy mozconfig
+printf " * copying mozconfig."						&&
+cp "${repodir}"/mightyluna-mozconfig-syslibs.config \
  "${srcdir}"/mozconfig							&&
+printf "\t\t\t--> done.\n"						&&
 
-export pkgname=palemoon							&&
+# d5 >> run sed from arch
+printf " * running sed."						&&
+sed -i 's#xlocale#locale#' \
+ "${srcdir}"/intl/icu/source/i18n/digitlst.cpp				&&
+printf "\t\t\t\t--> done.\n"						&&
+
+# d6 >> set mach vars
+export pkgname="mightyluna (palemoon)"					&&
 export pkgver=28.6.1							&&
 export MOZBUILD_STATE_PATH="$srcdir/mozbuild"				&&
 export MOZCONFIG="$srcdir/mozconfig"					&&
-export CPPFLAGS="$CPPFLAGS -O2"						&&
+export CPPFLAGS="$CPPFLAGS -O2 -Wno-format-overflow"			&&	
 
-printf "\npkgname\t\t: ${pkgname}\npkgver\t\t: ${pkgver}\n"		&&
-printf "srcdir\t\t: $srcdir\nmozbuild\t: ${MOZBUILD_STATE_PATH}\n"	&&
-printf "mozconfig\t: ${MOZCONFIG}\ncppflags\t: ${CPPFLAGS}\n"		&&
+# d6.1 >> print mach vars
+printf "\n + pkgname\t: ${pkgname}\n + pkgver\t: ${pkgver}\n"		&&
+printf " + srcdir\t: $srcdir\n + mozbuild\t: ${MOZBUILD_STATE_PATH}\n"	&&
+printf " + mozconfig\t: ${MOZCONFIG}\n + cppflags\t: ${CPPFLAGS}\n"	&&
 
-printf "\n * ready to run mach, shall we continue? (y/n):"
-read answer
- if [ "$answer" != "${answer#[Yy]}" ]
+# d7 >> check everything and ask to run mach
+printf "\n * ready to run mach, shall we continue? (y/n): "		&&
+read answerbuild
+ if [ "$answerbuild" != "${answerbuild#[Yy]}" ]
   then
-   cd "${srcdir}"	&&
-   vi mozconfig		&&
-   python2.7 mach build  &&
-   printf " * run mach install via su.\n"
- elif [ "$answer" != "${answer#[Nn]}" ];
-   then exit 0
- else " ! input Yy or Nn."; exit 1
+   cd "${srcdir}"							&&
+   vi mozconfig								&&
+   printf " * running mach.\n"						&&
+   printf " * tail -f /tmp/mightyluna-build.log to view progress.\n"	&&
+   python2.7 mach build &>> /tmp/mightyluna-build.log \
+   || printf " * build failed. check logs.\n"; rm -rfv ${srcdir}; exit 1
+  else
+   printf " * not running mach.\n"		&&
+   exit 0
+ fi
+
+# d8 >> ask to install build
+printf "\n * build complete, shall we run the installer? (y/n): "	&&
+read answerinst
+ if [ "$answerinst" != "${answerinst#[Yy]}" ]
+  then
+   printf " * installing.\n"			&&
+   su -c "python2.7 mach install"		&&
+   printf "\n * installation complete.\n"
+  else
+   printf "\n * not installing.\n"		&&
+   exit 0
  fi
