024dfe4726750e8ba1a981ed28d5aead05366709
must_eat <mss@tutanota.de>
2019-08-28 12:44:10 +0300

    upd: fetch, stripper; add: ldir; rmv: remuxsubs.

--- apathy-utils/remuxsubs.sh
+++
@@ -1,9 +0,0 @@
-#!/mss/bin/sh
-for file in *.mkv
-do
- meme=$(printf "$file\n" | sed 's/.mkv//g')
- mkvmerge -o "${meme}-subs.mkv" "${meme}.mkv" --language 0:eng --track-name 0:English "E${meme}".ass	\
-  --attachment-mime-type font/sfnt --attach-file '.ttf'							\
-  --attachment-mime-type application/vnd.ms-opentype --attach-file '.otf'				&&
- printf "\n\n done with episode $meme.\n\n\n"
-done 
