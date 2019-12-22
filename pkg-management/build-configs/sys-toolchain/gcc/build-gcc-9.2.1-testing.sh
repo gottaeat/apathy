788b459b5d38f02eef37b4879e8d39793d83575b
must_eat <mss@tutanota.de>
2019-12-22 16:15:26 +0300

    rew: gcc 9.2.1 snapshot build script, build without patches.

--- pkg-management/build-configs/sys-toolchain/gcc/build-gcc-9.2.1-testing.sh
+++ pkg-management/build-configs/sys-toolchain/gcc/build-gcc-9.2.1-testing.sh
@@ -1,6 +1,6 @@
 #!/mss/bin/sh
 # apathy-musl 1.1 - mss@waifu.club
-# build script for gcc9 snapshots.
+# build script for gcc9 snapshots, version 2.
 
 . /mss/bin/apathy-funcs
 
@@ -15,88 +15,45 @@ patchdir="/mss/repo/pkg-management/build-configs/sys-toolchain/gcc/patches"
 
 _gccver="${1}"
 _islver="${2}"
-
 _build="x86_64-apathy-linux-musl"
-_host="x86_64-apathy-linux-musl"
-_target="x86_64-apathy-linux-musl"
 
-# b2.1 >> set the patch applying func
-apply_patches(){
- _patches_Np1="libgnarl-musl.patch"
- _patches_Np0="
-  0010-ldbl128-config.patch
-  ada-shared.patch
-  fix-cxxflags-passing.patch
-  fix-musl-execinfo.patch
-  gccgo-musl.patch
-  invalid_tls_model.patch
-  libffi_gnulinux.patch
-  libgcc-musl-ldbl128-config.patch
-  musl-ada.patch
-  no-stack_chk_fail_local.patch
-  ppc64-pure64.patch"
-
- for pp in $_patches_Np0
-  do
-   aprint_ret "${c_blue}applying${c_reset}\t:${c_lcyan} ${pp}${c_reset}."
-   patch -Np0 -i "${patchdir}"/9.2.1/"${pp}" >>/tmp/gcc-build.log 2>&1
-   evalretkill
-  done
-
- for pp in $_patches_Np1
-  do
-   aprint_ret "${c_blue}applying\t${c_reset}:${c_lcyan} ${pp}${c_reset}."
-   patch -Np1 -i "${patchdir}"/9.2.1/"${pp}" >>/tmp/gcc-build.log 2>&1
-   evalretkill
-  done
-}
-
-# b2.2 >> set the configuring func
+# b2 >> set the configuring func
 _configure(){
-CFLAGS=" -pipe" \
-CXXFLAGS=" -pipe" \
-SED=sed \
-libat_cv_have_ifunc=no \
-../configure \
- --prefix=/usr \
- --build=${_build} \
- --host=${_host} \
- --target=${_target} \
- --with-pkgversion="apathy" \
+libat_cv_have_ifunc=no                            \
+../configure                                      \
+ --prefix=/usr                                    \
+ --build=${_build}                                \
  --with-bugurl="https://github.com/mssx86/apathy" \
- --with-system-zlib \
- --with-isl \
- --with-linker-hash-style=gnu \
- --enable-languages=c,c++ \
- --enable-threads=posix \
- --enable-clocale=generic \
- --enable-tls \
- --enable-libstdcxx-time \
- --enable-fully-dynamic-string \
- --enable-default-pie \
- --enable-default-ssp \
- --enable-vtable-verify \
- --enable-linker-build-id \
- --enable-fast-character \
- --disable-libstdcxx-pch \
- --disable-nls \
- --disable-multilib \
- --disable-symvers \
- --disable-libsanitizer \
- --disable-target-libiberty \
- --disable-libunwind-exceptions \
- --enable-checking=release \
+ --with-pkgversion="apathy"                       \
+ --with-isl                                       \
+ --with-system-zlib                               \
+ --disable-multilib                               \
+ --disable-symvers                                \
+ --disable-libmpx                                 \
+ --disable-libmudflap                             \
+ --disable-libsanitizer                           \
+ --disable-werror                                 \
+ --disable-fixed-point                            \
+ --disable-libstdcxx-pch                          \
+ --enable-linker-build-id                         \
+ --enable-checking=release                        \
+ --enable-languages=c,c++                         \
+ --enable-__cxa_atexit                            \
+ --enable-default-pie                             \
+ --enable-default-ssp                             \
+ --enable-shared                                  \
+ --enable-threads                                 \
+ --enable-tls                                     \
+ --mandir=/usr/share/man                          \
+ --infodir=/usr/share/info                        \
   >>/tmp/gcc-build.log 2>&1
 }
 
 # c1 >> print versions
 aprint_nc
-aprint "${c_blue}gcc${c_reset}\t\t: ${c_lcyan}${_gccver}${c_reset}."
-aprint "${c_blue}isl${c_reset}\t\t: ${c_lcyan}${_islver}${c_reset}."
-aprint_nc
-aprint "${c_blue}host${c_reset}\t\t: ${c_lcyan}${_host}${c_reset}."
-aprint "${c_blue}build${c_reset}\t: ${c_lcyan}${_build}${c_reset}."
-aprint "${c_blue}target${c_reset}\t: ${c_lcyan}${_target}${c_reset}."
+lsdetail "gcc version" "${_gccver}"
+lsdetail "isl version" "${_islver}"
+lsdetail "build"       "${_build}"
 aprint_nc
 
 # c2.1 >> extract gcc sauce
@@ -104,8 +61,8 @@ if [ ! -f "${saucedir}/${_gccver}.tar.xz" ]
  then
   aprint_fail "gcc tarball does not exist in saucedir, exiting."; exit 1
  else
- aprint_ret "${c_blue}extracting${c_reset}\t: the ${c_lcyan}gcc source${c_reset} to current directory."
-  tar xf "${saucedir}"/"${_gccver}".tar.xz >/tmp/gcc-build.log 2>&1
+  aprint_ret "${c_blue}extracting${c_reset}\t: the ${c_lcyan}gcc source${c_reset} to current directory."
+   tar xf "${saucedir}"/"${_gccver}".tar.xz >/tmp/gcc-build.log 2>&1
   evalretkill
   
   cd ${_gccver}
@@ -123,18 +80,14 @@ if [ ! -f "${saucedir}/${_islver}.tar.xz" ]
   mv -v "${_islver}" "isl" >>/tmp/gcc-build.log 2>&1
 fi 
 
-# d1 >> apply the patches
-aprint_nc
-aprint "applying the patches."
-apply_patches
-
-# d2.1 >> change the default libdir to /lib from /lib64
+# d1 >> change the default libdir to /lib from /lib64
 aprint_nc
 aprint_ret "running sed to change the default libdir to ${c_lcyan}/lib${c_reset}."
- sed -e '/m64=/s/lib64/lib/' -i.orig gcc/config/i386/t-linux64
+ sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64
+ sed -i 's/lib64/lib/'       gcc/config/i386/linux64.h
 evalretkill
 
-# d.2 >> _FORTIFY_SOURCE needs an optimization level.
+# d2 >> _FORTIFY_SOURCE needs an optimization level.
 aprint_ret "${c_lcyan}_FORTIFY_SOURCE${c_reset} needs an optimization level."
  sed -i "/ac_cpp=/s/\$CPPFLAGS/\$CPPFLAGS -O2/" gcc/configure libiberty/configure
 evalretkill
@@ -159,9 +112,7 @@ read answerbuildgcc
 if [ "$answerbuildgcc" != "${answerbuildgcc#[Yy]}" ]
  then
   aprint_ret "running make, tail -f /tmp/gcc-build.log to view."
-
-  make -j$(($(nproc)+1)) \
-   CFLAGS=" -pipe" CXXFLAGS=" -pipe" >>/tmp/gcc-build.log 2>&1
+   make -j$(($(nproc)+1)) >>/tmp/gcc-build.log 2>&1
   evalretkill
 
   aprint_nc
