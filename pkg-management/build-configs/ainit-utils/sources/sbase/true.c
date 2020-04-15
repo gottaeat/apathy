61176efcc8127e7c0ca18de571adb4c774e048c5
must_eat <mss@tutanota.de>
2020-04-15 20:27:13 +0300

    upd: (ainit-utils) tarball up the modified tool sources.

--- pkg-management/build-configs/ainit-utils/sources/sbase/true.c
+++
@@ -1,6 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-int
-main(void)
-{
-	return 0;
-}
--- pkg-management/build-configs/ainit-utils/sources/ubase/passwd.h
+++
@@ -1,4 +0,0 @@
-/* See LICENSE file for copyright and license details. */
-/* passwd.c */
-int pw_check(const struct passwd *, const char *);
-int pw_init(void);
