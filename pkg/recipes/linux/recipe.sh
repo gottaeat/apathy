4270701de399f3b6a85f9846dfb84d2306d66b24
must_eat <mss@tutanota.de>
2022-01-11 18:51:33 +0300

    upd: (recipes/) linux/: > bump to 5.16.0. > files/x230.config; regen for 5.16. > patches/:  - nuke:   > 0003-bring-back-fbcon-scrollback.patch   > 0004-set-rq_affinity-to-force-full-multithreading-I-O-req.patch   > 0007-add-zstd-improvements-from-xanmod.patch  - port the rest of the patches.

--- pkg/recipes/linux/recipe.sh
+++ pkg/recipes/linux/recipe.sh
@@ -1,6 +1,6 @@
 #!/mss/bin/sh
 # apathy musl 1.2 - mss@tutanota.de
-# build script for 5.15 kernels, version 1, for thinkpad t61 and x230.
+# build script for 5.16 kernels, version 1, for thinkpad t61 and x230.
 
 . /mss/files/funcs
 . /mss/files/device.conf
@@ -26,7 +26,7 @@ fi
       rdr="${logdir}/${blddate}-linux-${kerver}.log"
 
   useconf="${repodir}/files/${amachine}.config"
- localver="5.15$(awk '/LOCALVERSION=/{gsub(/CONFIG_LOCALVERSION=|\"/,"");\
+ localver="5.16$(awk '/LOCALVERSION=/{gsub(/CONFIG_LOCALVERSION=|\"/,"");\
                       print}' ${useconf})"
   bldsalt="$(awk     '/BUILD_SALT=/{gsub(/CONFIG_BUILD_SALT=|\"/,"");    \
                       print}' ${useconf})"
